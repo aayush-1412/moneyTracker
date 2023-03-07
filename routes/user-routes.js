fe5bcafc965bd71c75b7f59eaa26a69f575de867
Aayush <gaayushg11@gmail.com>
2023-03-07 11:52:50 +0530

    Fixed some bugs

--- routes/user-routes.js
+++
@@ -1,9 +0,0 @@
-import express from "express"
-import { getAllUser } from "../server/controllers/user-controller.js";
-import { signup } from "../server/controllers/user-controller.js";
-import { login } from "../server/controllers/user-controller.js";
-const router =express.Router();
-router.get("/",getAllUser)
-router.post("/signup",signup)
-router.post("/login",login)
-export default router
